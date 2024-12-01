25878acec6ea97845d5ebbfcd41f1bab06ca1384
DaYaParolNePomnu <makakakrutaya8@gmail.com>
2024-12-01 14:53:32 +0800

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HelloWorld");                  //Выводит HelloWorld 1 раз
+    }
+
+    // Update is called once per frame
+    void Update( )
+    {
+   
+    }
+}
